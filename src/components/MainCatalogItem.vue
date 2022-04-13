3238c33873aae758168e9628760eb5f3c613fa34
Aleksandr <aleksaandr24@gmail.com>
2022-04-13 14:52:41 +0500

    Add Main Catalog Item

--- src/components/MainCatalogItem.vue
+++ src/components/MainCatalogItem.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="main-catalog__item">
-    
+    <div class="item__image">
+      <img src="@/assets/images/item-image.png" width="353" height="246" alt="item_image">
+    </div>
+    <div class="item__name">
+      Стул рабочий
+    </div>
+    <div class="item__cost">
+      6 000 &#8381;
+    </div>
+    <div class="item__cart-button">
+      <button class="cart-button">Добавить в корзину</button>
+    </div>
   </div>
 </template>
 
