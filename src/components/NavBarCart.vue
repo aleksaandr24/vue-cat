34a86b11c34521004e4cc0a15762a0d28d7acdb0
Aleksandr <aleksaandr24@gmail.com>
2022-04-18 17:09:20 +0500

    Add ModalContainer, NavBarCart click handler

--- src/components/NavBarCart.vue
+++ src/components/NavBarCart.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="navbar__cart">
-    <div class="cart__icon">
+    <div id="show-modal" @click="showModal = true" class="cart__icon">
       <div v-if="this.$store.state.shopCart.length > 0" class="cart__bubble">
         {{ this.$store.state.shopCart.length }}
       </div>
     </div>
   </div>
+  <Teleport to="body">
+    <!-- use the modal component, pass in the prop -->
+    <ModalContainer :show="showModal" @close="showModal = false">
+      <template #header>
+        <h3>Оформить заказ</h3>
+      </template>
+    </ModalContainer>
+  </Teleport>
 </template>
 
 <script>
+import ModalContainer from '@/components/ModalContainer.vue'
+
 export default {
   name: 'NavBarCart',
+  components: {
+    ModalContainer
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  },
   computed: {
     cartIndicator() {
       return true
