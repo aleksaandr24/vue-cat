deca6798869f0210f6946977c236f69bc4f6c0a9
Aleksandr <aleksaandr24@gmail.com>
2022-05-28 16:58:52 +0500

    Add update ModalDetailedBody, ReviewsRating, CatalogPreloader, NotFoundPage components code and styles

--- src/components/ModalDetailedBody.vue
+++ src/components/ModalDetailedBody.vue
@@ -1,88 +1,222 @@
 <template>
-  <div class="modal-detailed__item-image">
-    <img :src="catalogItemFull.img" alt="item_image">
+  <div class="detailed-pic">
+    <img
+      :src="catalogItemFull.img"
+      alt="item_image"
+      class="detailed-pic__img"
+    >
   </div>
-  <div class="modal-detailed__item-name">
+  <div class="detailed-name">
     {{ catalogItemFull.name }}
   </div>
-  <div class="modal-detailed__item-price">
+  <div class="detailed-price">
     {{ catalogItemFull.price.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'}) }}
   </div>
-  <Tabs :options="{ useUrlFragment: false }" wrapper-class="modal-detailed__item-tabs-wrapper" panels-wrapper-class="item-tabs__panels" nav-class="item-tabs__nav" nav-item-class="item-tabs__item" nav-item-active-class="item-tabs__item_active" nav-item-link-class="item-tabs__link" nav-item-link-active-class="item-tabs__link_active">
-    <Tab id="item_description" name="Описание" panel-class="item_tabs__panel">
+  <Tabs
+    :options="{ useUrlFragment: false }"
+    wrapper-class="item-tabs"
+    panels-wrapper-class="item-tabs__panels"
+    nav-class="item-tabs__nav"
+    nav-item-class="item-tabs__item"
+    nav-item-active-class="item-tabs__item_active"
+    nav-item-link-class="item-tabs__link"
+    nav-item-link-active-class="item-tabs__link_active"
+  >
+    <Tab
+      panel-class="item_tabs__panel"
+      id="item-description"
+      name="Описание"
+    >
       <div v-html="catalogItemFull.descr"></div>
     </Tab>
-    <Tab id="item_properties" name="Характеристики">
-      <div v-for="(prop, index) in catalogItemFull.props" :key="index" class="item_properties__row">
-        <div>{{ prop.caption }}</div>
-        <div class="item_properties__row-value">{{ prop.value }}&nbsp;{{ prop.measure }}</div>
+    <Tab
+      id="item-properties"
+      name="Характеристики"
+    >
+      <div
+        v-for="(prop, index) in catalogItemFull.props"
+        :key="index"
+        class="item-properties__row"
+      >
+        <div class="item-properties__row-caption">
+          {{ prop.caption }}
+        </div>
+        <div class="item-properties__row-value">
+          {{ prop.value }}&nbsp;{{ prop.measure }}
+        </div>
       </div>
     </Tab>
-    <Tab id="item_reviews" name="Отзывы">
+    <Tab
+      id="item-reviews"
+      name="Отзывы"
+    >
       <div v-if="catalogItemFull.reviews.length > 0">
-        <div v-for="(prop, index) in catalogItemFull.reviews.slice().reverse()" :key="index" class="item_reviews__row">
-          <div class="item_reviews__avatar">
-            <img :src="prop.avatar" alt="avatar">
+        <div
+          v-for="(prop, index) in catalogItemFull.reviews.slice().reverse()"
+          :key="index"
+          class="item-reviews__row"
+        >
+          <div class="item-reviews__avatar">
+            <img
+              :src="prop.avatar"
+              alt="avatar"
+              class="item-reviews__img"
+            >
           </div>
           <div>
-            <div class="item_reviews__author">
-              <div class="item_reviews__author-name">{{ prop.author }}</div>
-              <div class="item_reviews__author-rate">
+            <div class="item-reviews__author">
+              <div class="item-reviews__author-name">
+                {{ prop.author }}
+              </div>
+              <div class="item-reviews__author-rate">
                 <ReviewRating :rate="prop.rate"/>
               </div>
             </div>
-            <div class="item_reviews__text">
+            <div class="item-reviews__text">
               {{ prop.text }}
             </div>
           </div>
         </div>
       </div>
-      <div v-else class="item_reviews__author-name">Отзывов нет</div>
+      <div
+        v-else
+        class="item-reviews__author-name"
+      >
+        Отзывов нет
+      </div>
     </Tab>
-    <Tab id="item_feedback-review" name="Оставить отзыв">
+    <Tab
+      id="item_feedback-review"
+      name="Оставить отзыв"
+    >
       <div v-if="!reviewSended">
-        <form @submit.prevent="addReview" class="review-form">
+        <form
+          @submit.prevent="addReview"
+          class="review-form"
+        >
           <div>
             <div class="review-form__rate">Оценка</div>
             <div class="rating-area">
-              <input type="radio" id="star-5" v-model="reviewFormRating" @change="v$.reviewFormRating.$touch" value="5">
-              <label for="star-5" title="Оценка 5"></label>	
-              <input type="radio" id="star-4" v-model="reviewFormRating" @change="v$.reviewFormRating.$touch" value="4">
-              <label for="star-4" title="Оценка 4"></label>    
-              <input type="radio" id="star-3" v-model="reviewFormRating" @change="v$.reviewFormRating.$touch" value="3">
-              <label for="star-3" title="Оценка 3"></label>  
-              <input type="radio" id="star-2" v-model="reviewFormRating" @change="v$.reviewFormRating.$touch" value="2">
-              <label for="star-2" title="Оценка 2"></label>    
-              <input type="radio" id="star-1" v-model="reviewFormRating" @change="v$.reviewFormRating.$touch" value="1">
-              <label for="star-1" title="Оценка 1"></label>
+              <input
+                v-model="reviewFormRating"
+                @change="v$.reviewFormRating.$touch"
+                type="radio"
+                id="star-5"
+                value="5"
+              >
+              <label
+                for="star-5"
+                title="Оценка 5"
+              >
+              </label>	
+              <input
+                v-model="reviewFormRating"
+                @change="v$.reviewFormRating.$touch"
+                type="radio"
+                id="star-4"
+                value="4"
+              >
+              <label
+                for="star-4"
+                title="Оценка 4"
+              >
+              </label>    
+              <input
+                v-model="reviewFormRating"
+                @change="v$.reviewFormRating.$touch"
+                type="radio"
+                id="star-3"
+                value="3"
+              >
+              <label
+                for="star-3"
+                title="Оценка 3"
+              >
+              </label>  
+              <input
+                v-model="reviewFormRating"
+                @change="v$.reviewFormRating.$touch"
+                type="radio"
+                id="star-2"
+                value="2"
+              >
+              <label
+                for="star-2"
+                title="Оценка 2"
+              >
+              </label>    
+              <input
+                v-model="reviewFormRating"
+                @change="v$.reviewFormRating.$touch"
+                type="radio"
+                id="star-1"
+                value="1"
+              >
+              <label
+                for="star-1"
+                title="Оценка 1"
+              >
+              </label>
             </div>
             <div>
-              <span v-if="!v$.reviewFormRating.$dirty" class="review-form__error">
-              {{ v$.reviewFormRating.required.$message }}
+              <span
+                v-if="!v$.reviewFormRating.$dirty"
+                class="review-form__error"
+              >
+                {{ v$.reviewFormRating.required.$message }}
               </span>
             </div>
           </div>
           <div class="review-form__name">
             <label for="review-name">Имя</label><br>
-            <input type="text" id="review-name" v-model.trim="reviewFormName" placeholder="Имя" :class="v$.reviewFormName.$invalid ? 'review-form__input review-form__input_error' : 'review-form__input'"><br>
-            <span v-if="v$.reviewFormName.$invalid" class="review-form__error">
+            <input
+              v-model.trim="reviewFormName"
+              :class="v$.reviewFormName.$invalid ? 'review-form__input review-form__input_error' : 'review-form__input'"
+              type="text"
+              id="review-name"
+              placeholder="Имя"
+            >
+            <br>
+            <span
+              v-if="v$.reviewFormName.$invalid"
+              class="review-form__error"
+            >
               {{ v$.reviewFormName.required.$message }}
             </span>
           </div>
           <div class="review-form__text">
             <label for="review-text">Отзыв</label><br>
-            <textarea rows="10" id="review-text" v-model.trim="reviewFormText" placeholder="Отзыв" :class="v$.reviewFormName.$invalid ? 'review-form__textarea review-form__textarea_error' : 'review-form__textarea'"></textarea><br>
-            <span v-if="v$.reviewFormText.$invalid" class="order-form__error">
+            <textarea
+              v-model.trim="reviewFormText"
+              :class="v$.reviewFormName.$invalid ? 'review-form__textarea review-form__textarea_error' : 'review-form__textarea'"
+              rows="10"
+              id="review-text"
+              placeholder="Отзыв"
+            >
+            </textarea>
+            <br>
+            <span
+              v-if="v$.reviewFormText.$invalid"
+              class="order-form__error"
+            >
               {{ v$.reviewFormText.required.$message }}
             </span>
           </div>
-          <div class="review-form__button">
-            <button type="submit">Отправить отзыв</button>
+          <div class="review-form__submit">
+            <button
+              type="submit"
+              class="review-form__submit-button"
+            >
+              Отправить отзыв
+            </button>
           </div>
         </form>
       </div>
-      <div v-else class="review-form__recieve">
-        <h2>Отзыв получен</h2>
+      <div
+        v-else
+        class="review-form__recieve"
+      >
+        <h2 class="review-form__recieve-title">Отзыв получен</h2>
       </div>
     </Tab>
   </tabs>
--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -4,7 +4,12 @@
     <p class="not-found__text">The page you are looking for does not exist.</p>
     <p class="not-found__text">How you got here is a mystery.</p>
     <p class="not-found__text">But you can click the button below to go back to the homepage.</p>
-    <button @click="goHome" class="not-found__button">HOME</button>
+    <button
+      @click="goHome"
+      class="not-found__button"
+    >
+      HOME
+    </button>
   </div>
 </template>
 
